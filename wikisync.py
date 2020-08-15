7b4164229f2ae920b0e652b71bd15b6cd850f2dd
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-15 06:55:05 +0530

    Initial release

---
+++ wikisync.py
@@ -0,0 +1,8 @@
+import os
+import igem_wikisync as sync
+
+sync.run(
+    team      = os.environ.get('WIKISYNC_TEAM'),
+    src_dir   = os.environ.get('WIKISYNC_SOURCE'),
+    build_dir = os.environ.get('WIKISYNC_BUILD')
+)
