003eeb8157b71cbc850994a2c357508400d4ccac
Pranav Ballaney <ballaneypranav@gmail.com>
2020-11-01 17:47:01 +0530

    Add support for poster mode

--- wikisync.py
+++ wikisync.py
@@ -4,5 +4,6 @@ import igem_wikisync as sync
 sync.run(
     team      = os.environ.get('WIKISYNC_TEAM'),
     src_dir   = os.environ.get('WIKISYNC_SOURCE'),
-    build_dir = os.environ.get('WIKISYNC_BUILD')
+    build_dir = os.environ.get('WIKISYNC_BUILD'),
+    poster_mode = os.environ.get('WIKISYNC_POSTER')
 )
